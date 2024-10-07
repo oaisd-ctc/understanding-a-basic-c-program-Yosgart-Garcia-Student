3dafb3a32bb4f76fa83b7f9f10984484fe35b0b0
Yosgart-Garcia-Student <yoga1aa@student.oaisd.org>
2024-10-07 16:54:23 +0000

    Added a C# program that can print out a message using variables.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World !");
+        Console.WriteLine("My name isn't Emmanuel Martinez, no clue who that guy is.");
+
+        int age = 16;
+        string greeting = "Hello World";
+        string firstName = "Yosgart";
+        string lastName = "Garcia";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! My name is " + firstName + " " + lastName + ". I am " + age + " years old. I just spent $" + money + " on rice pudding.");
+    }
+}
